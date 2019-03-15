862b700d912e9136872c6c0009bc3e2da5744bda
Colin Smith <colinsmith@MacBook-Pro-6.local>
2019-03-15 12:35:47 -0600

    first commit

---
+++ Model/Item+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Item+Convenience.swift
+//  Cart
+//
+//  Created by Colin Smith on 3/15/19.
+//  Copyright © 2019 Colin Smith. All rights reserved.
+//
+
+import Foundation
---
+++ ViewController/ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  Cart
+//
+//  Created by Colin Smith on 3/15/19.
+//  Copyright © 2019 Colin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
